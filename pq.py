0aa8b3178dff02434dc8d89a2c8d35c21722de19
Сергей <seggiy@gmail.com>
2026-01-15 21:46:51 +0500

    GUI реализован: загрузка данных - файл экспорта из электронного журнала, управление сервером - web сервер для регистрации выбора предметов ВсОШ (для классных руководителей) с сохранением результатов в файл olympiad_data.json, Экспорт данных - формирование заявлений из шаблона с заменой маркеров в тексте на ФИО и выбранные предметы и выгрузка результатов выбора предметов в xlsx файл (каждый лис = предмет с перечислением класс-ученик) и листом общей статистики, окно планового процента участия по классам. Версия 0.1 альфа. Отправка не тестирование.

--- pq.py
+++ pq.py
@@ -2,4 +2,10 @@ import pandas as pd
 
 sheets = pd.read_excel("МЭ 25-26.xlsx", sheet_name=None)
 res = pd.concat(sheets.values(), ignore_index=True)
+res = res[res["Статус"].isin(['Победитель', 'Призёр'])]
+res['Результат'] = res['Предмет'] + ' ('+res['Статус'] + ')'
+res['Класс участника'] = res['Класс участника'].str.upper().str.replace(":", "", regex=False)
+res[["Код", "ОО"]] = res["Школа"].str.split(" - ", n=1, expand=True)
+res = res[["Код", "ОО", 'Класс участника', 'Участник', 'Результат']]
+
 res.to_excel("res МЭ 25-26.xlsx", index=False)
